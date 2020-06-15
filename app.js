b92cf72dddb2df880ed18aad6a18da40922639bf
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-15 13:23:47 +0300

    добавила cors

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet'); // доп защита Экспресс-приложения. Устанавливает нужные заголовки для HTTP
+const cors = require('cors'); // библиотека, чтобы сайт мог отправлять запросы
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const errorsHandler = require('./middlewares/errorHandler');
 const { CONNECTION_ADDRESS, PORT, RATE_LIMIT } = require('./config');
 
+const whitelist = [' https://news-explorer-api.gq/', ' http://news-explorer-api.gq/', 'https://www.news-explorer-api.gq/', 'https://www.news-explorer-api.gq/'];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 mongoose.connect(CONNECTION_ADDRESS, {
   useNewUrlParser: true,
@@ -21,6 +33,10 @@ mongoose.connect(CONNECTION_ADDRESS, {
 });
 
 const app = express();
+app.get('/', cors(corsOptions), (req, res, next) => {
+  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' });
+  next();
+});
 app.use(cookieParser());
 app.use(helmet());
 app.use(rateLimit(RATE_LIMIT));
