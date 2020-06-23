a18bd64fe83f9cdaab6826ffd580cfdf433f9aff
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-24 00:37:46 +0300

    Настроила корс для сайта 4

--- controllers/users.js
+++ controllers/users.js
@@ -41,9 +41,9 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, SECRET);
       res
         .cookie('jwt', token, {
-          domain: '',
           maxAge: 604800,
           httpOnly: true,
+          sameSite: true,
         })
         .end();
     })
