07970f8170c7199039a3603cb008b19090c7d184
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-24 15:13:21 +0300

    проверяю, что не так с auth 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET);
   } catch (err) {
-    const error = new UnauthorizedError( token); // удали потом
+    const error = new UnauthorizedError('Необходима авторизация');
     next(error);
   }
   req.user = payload; // записываем пейлоуд в объект запроса
