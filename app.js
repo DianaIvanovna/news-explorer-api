84cb3d3c1e15290fd2ede88f367758ff0d6391f2
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-24 14:44:28 +0300

    Настроила корс для сайта 15

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ mongoose.connect(CONNECTION_ADDRESS, {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-// 'https://news-explorer-api.gq/' || 'http://news-explorer-api.gq/' || 'https://www.news-explorer-api.gq/' || 'https://www.news-explorer-api.gq/',
+const whitelist = ['https://news-explorer-api.gq', 'http://news-explorer-api.gq', 'https://www.news-explorer-api.gq', 'https://www.news-explorer-api.gq'];
 
 const corsOptions = {
-  origin: 'https://news-explorer-api.gq' || 'http://news-explorer-api.gq' || 'https://www.news-explorer-api.gq' || 'https://www.news-explorer-api.gq',
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
 
